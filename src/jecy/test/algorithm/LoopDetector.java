ffbb0412cae5a5082fecfcf20dd9fbc91f4e7e5b
jecy-8 <pliny.tingzhan@hotmail.com>
2020-03-30 17:00:41 +0800

    add one more method to detect loop in a list

--- src/jecy/test/algorithm/LoopDetector.java
+++ src/jecy/test/algorithm/LoopDetector.java
@@ -1,8 +1,13 @@
 package jecy.test.algorithm;
 
+import java.util.HashMap;
+
 public class LoopDetector {
 
-    public static boolean hasLoop(Node headNode) {
+    private static HashMap<Node, Integer> nodeMap = new HashMap<>();
+
+    public static boolean hasLoopByPointer(Node headNode) {
+        // 空链表或者只有一个节点，肯定没有环
         if (headNode == null || headNode.getNext() == null) {
             return false;
         }
@@ -24,6 +29,20 @@ public class LoopDetector {
         return false;
     }
 
+    public static boolean hasLoopByMap(Node headNode, int index) {
+        // 空链表或者只有一个节点，肯定没有环
+        if (headNode == null || headNode.getNext() == null) {
+            return false;
+        }
+
+        if (nodeMap.containsKey(headNode)) {
+            return true;
+        } else {
+            nodeMap.put(headNode, index);
+            return hasLoopByMap(headNode.getNext(), ++ index);
+        }
+    }
+
 
     public static void main(String[] args) {
         // 创建一个有loop的链表，尾结点e指向中间节点c
@@ -40,11 +59,18 @@ public class LoopDetector {
         p4.setNext(p2);
         // 链表中含有环，直接调用listAllNodes会出现无限死循环
 //        System.out.println("Primary list is: " + NodeUtils.listAllNodes(head));
-        if (hasLoop(head)) {
+
+        if (hasLoopByPointer(head)) {
             System.out.println("Loop detected inside list.");
         } else {
             System.out.println("No loop detected.");
         }
 
+        // 用足迹法
+        if (hasLoopByMap(head, 0)) {
+            System.out.println("Loop detected inside list.");
+        } else {
+            System.out.println("No loop detected.");
+        }
     }
 }
